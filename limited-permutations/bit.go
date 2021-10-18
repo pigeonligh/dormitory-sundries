407f70cdffb3705ebb49d1ec1431c13ff3c03db1
pigeonligh <pigeonligh@hotmail.com>
2021-10-18 16:15:56 +0800

    limited permutations add print

--- limited-permutations/bit.go
+++ limited-permutations/bit.go
@@ -2,9 +2,25 @@ package main
 
 import "fmt"
 
+const P = false
+
 type order struct {
 	u int
 	v int
+}
+
+var now []int
+
+func printAll(ni, status int, ways [][]int) {
+	if status == 0 {
+		fmt.Println(now)
+		return
+	}
+	for _, v := range ways[status] {
+		to := status ^ (1 << v)
+		now[ni] = v + 1
+		printAll(ni+1, to, ways)
+	}
 }
 
 func Permutations(n int, ords []order) int64 {
@@ -13,11 +29,12 @@ func Permutations(n int, ords []order) int64 {
 		e[i] = make([]bool, n)
 	}
 	for _, ord := range ords {
-		e[ord.u-1][ord.v-1] = true
+		e[ord.v-1][ord.u-1] = true
 	}
 
 	m := 1 << n
 	f := make([]int64, m)
+	ways := make([][]int, m)
 	f[0] = 1
 	for s := 1; s < m; s++ {
 		for i := 0; i < n; i++ {
@@ -34,16 +51,21 @@ func Permutations(n int, ords []order) int64 {
 					add = false
 				}
 			}
-			if add {
+			if add && f[t] > 0 {
 				f[s] += f[t]
+				ways[s] = append(ways[s], i)
 			}
 		}
+	}
+	now = make([]int, n)
+	if P {
+		printAll(0, m-1, ways)
 	}
 	return f[m-1]
 }
 
 func main() {
-	n := 16
+	n := 12
 	ords := make([]order, 0)
 	for i := 1; i < n; i++ {
 		ords = append(ords, order{i, n})
